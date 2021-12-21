a9c9aad896527dc58c6b400ca60e59fab060aee9
Phong <xadkile@gmail.com>
2021-12-21 21:34:51 +0700

    misc

--- src/bicp_document_structure/worksheet/UserFriendlyWorksheet.py
+++ src/bicp_document_structure/worksheet/UserFriendlyWorksheet.py
@@ -3,7 +3,6 @@ from typing import Tuple, Union
 
 from bicp_document_structure.cell.address.CellAddress import CellAddress
 from bicp_document_structure.range.Range import Range
-from bicp_document_structure.range.RangeImp import RangeImp
 from bicp_document_structure.range.address.RangeAddress import RangeAddress
 
 
@@ -13,13 +12,3 @@ class UserFriendlyWorksheet(ABC):
 
     def range(self, rangeAddress: Union[str, RangeAddress, Tuple[CellAddress, CellAddress]]) -> Range:
         raise NotImplementedError()
-        # parsedAddress = rangeAddress
-        # if isinstance(rangeAddress, str):
-        #     parsedAddress = RangeLabel(rangeAddress)
-        #
-        # if isinstance(rangeAddress, Tuple):
-        #     ad1 = rangeAddress[0]
-        #     ad2 = rangeAddress[1]
-        #     parsedAddress = RangeAddressImp(ad1, ad2)
-
-        return RangeImp.fromRangeAddress(parsedAddress, self)
