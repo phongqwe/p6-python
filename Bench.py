e639e2b40e0f57046435c74256d5d74361c9f7b4
Phong <xadkile@gmail.com>
2022-02-14 11:26:50 +0700

    misc: - add .gitignore

--- Bench.py
+++ Bench.py
@@ -1,22 +1,9 @@
 import unittest
-from pathlib import Path
 
-from bicp_document_structure.workbook.WorkbookKeyImp import WorkbookKeyImp
+from jupyter_client import KernelClient
 
-x=123
 
-def zsd():
-    return 10
-
-def execz(f):
-    print(f())
 class Bench(unittest.TestCase):
     def test_z(self):
-
-        d = {
-            WorkbookKeyImp("1",Path("11")): 1,
-            WorkbookKeyImp("2",Path("22")):2
-        }
-
-        ek = WorkbookKeyImp("2",Path("22"))
-        print(ek in d.keys())
+        client = KernelClient()
+        client.shell_channel
