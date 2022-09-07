dd7578fcd5ef08704dda4e89b768c303bca93888
qxdzbc <31875822+qxdzbc@users.noreply.github.com>
2022-09-07 13:38:55 +0700

    update: - misc

--- src/com/qxdzbc/p6/new_architecture/rpc/cell/RpcCell.py
+++ src/com/qxdzbc/p6/new_architecture/rpc/cell/RpcCell.py
@@ -7,24 +7,36 @@ from com.qxdzbc.p6.document_structure.communication.event.data_structure.SingleS
     SingleSignalResponse
 from com.qxdzbc.p6.document_structure.util.report.error.ErrorReport import ErrorReport
 from com.qxdzbc.p6.document_structure.util.result.Result import Result
-from com.qxdzbc.p6.document_structure.util.result.Results import Results
 from com.qxdzbc.p6.document_structure.workbook.key.WorkbookKey import WorkbookKey
 from com.qxdzbc.p6.new_architecture.common.RpcUtils import RpcUtils
 from com.qxdzbc.p6.new_architecture.rpc.StubProvider import RpcStubProvider
 from com.qxdzbc.p6.new_architecture.rpc.cell.msg.CopyCellRequest import CopyCellRequest
-from com.qxdzbc.p6.new_architecture.rpc.data_structure.CellId import CellId
 from com.qxdzbc.p6.new_architecture.rpc.data_structure.CellValue import CellValue
 from com.qxdzbc.p6.new_architecture.rpc.data_structure.StrMsg import StrMsg
 from com.qxdzbc.p6.proto.rpc.cell.service.CellService_pb2_grpc import CellServiceStub
 
 
 class RpcCell(Cell):
+
+    def __init__(
+            self,
+            cellAddress: CellAddress,
+            wbKey: WorkbookKey,
+            wsName: str,
+            stubProvider: RpcStubProvider,
+    ):
+        self._address = cellAddress
+        self._wbk = wbKey
+        self._wsName = wsName
+        self._sp = stubProvider
+
     @property
     def displayValue(self) -> str:
-        def f()->str:
+        def f() -> str:
             oProto = self._cellSv.getDisplayValue(request = self.id.toProtoObj())
             o = StrMsg.fromProto(oProto)
             return o.v
+
         return self._onCellSvOk(f)
 
     @property
@@ -41,11 +53,12 @@ class RpcCell(Cell):
             oProto = self._cellSv.getFormula(request = self.id.toProtoObj())
             o = StrMsg.fromProto(oProto)
             return o.v
+
         return self._onCellSvOk(f)
 
     @property
     def bareValue(self):
-        cv:CellValue = self.cellValue
+        cv: CellValue = self.cellValue
         return cv.value
 
     @property
@@ -55,22 +68,24 @@ class RpcCell(Cell):
     def isEmpty(self):
         return self.cellValue.isEmpty()
 
-    def reRunRs(self) ->Result[None,ErrorReport]:
+    def reRunRs(self) -> Result[None, ErrorReport]:
         def f():
             oProto = self._cellSv.reRun(request = self.id.toProtoObj())
             o = SingleSignalResponse.fromProto(oProto)
             return o.toRs()
+
         return self._onCellSvOkRs(f)
 
-    def copyFromRs(self, anotherCell: "Cell")->Result[None,ErrorReport]:
+    def copyFromRs(self, anotherCell: "Cell") -> Result[None, ErrorReport]:
         def f():
             request = CopyCellRequest(
                 fromCell = anotherCell.id,
                 toCell = self.id
             )
-            oProto = self._cellSv.copyFrom(request=request.toProtoObj())
+            oProto = self._cellSv.copyFrom(request = request.toProtoObj())
             o = SingleSignalResponse.fromProto(oProto)
             return o.toRs()
+
         return self._onCellSvOkRs(f)
 
     @property
@@ -83,23 +98,12 @@ class RpcCell(Cell):
             oProto = self._cellSv.getCellContent(request = self.id.toProtoObj())
             o = CellContent.fromProto(oProto)
             return o
+
         return self._onCellSvOk(f)
 
     @property
     def address(self) -> CellAddress:
         return self._address
-
-    def __init__(
-            self,
-            cellAddress: CellAddress,
-            wbKey: WorkbookKey,
-            wsName: str,
-            stubProvider: RpcStubProvider,
-    ):
-        self._address = cellAddress
-        self._wbk = wbKey
-        self._wsName = wsName
-        self._sp = stubProvider
 
     @property
     def _cellSv(self) -> Optional[CellServiceStub]:
@@ -117,6 +121,5 @@ class RpcCell(Cell):
             oProto = self._cellSv.getCellValue(request = self.id.toProtoObj())
             o = CellValue.fromProto(oProto)
             return o
+
         return self._onCellSvOk(f)
-
-
